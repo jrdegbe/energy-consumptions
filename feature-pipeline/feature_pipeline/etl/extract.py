eef36cd19336f3cdcc4aa806fefff1fa37c0d88d
DataMasters Bootcamp <derek@jrdegbe.com>
2024-01-18 21:20:53 +0000

    created a feature_pipeline and ETL dir. with scripts in extract.py

---
+++ feature-pipeline/feature_pipeline/etl/extract.py
@@ -0,0 +1,12 @@
+import datetime
+from json import JSONDecodeError
+from pathlib import Path
+from pandas.errors import EmptyDataError  
+from typing import Any, Dict, Tuple, Optional
+
+
+import pandas as pd
+import requests
+
+from yarl import URL
+
